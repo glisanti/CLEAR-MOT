f20c60140a4a99f69d0f933331951b69eabff95f
glisanti <glisanti@gmail.com>
2013-04-17 15:56:22 +0300

    added file demo

---
+++ demo.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) Copyright 2013 - MICC - Media Integration and Communication Center,
+% University of Florence. 
+% Iacopo Masi and Giuseppe Lisanti  <masi,lisanti> @dsi.unifi.it
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear
+% groundtruth and results are examples. Ricreate these two structures if
+% you wanto to use it in your own multi-target tracker.
+load groundtruth
+load result
+VOCscore = 0.5;
+dispON  = true;
+ClearMOT = evaluateMOT(gt,result,VOCscore,dispON);
